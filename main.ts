18fd279b26d05dba2e6ba2ba3678683eb8119288
Lorlokus <128829956+Lorlokus@users.noreply.github.com>
2023-03-24 17:11:04 +0100

    Tijereas+Papel

--- main.ts
+++ main.ts
@@ -1,16 +1,139 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("Samuel")
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.showString("Lorenzo Da Mota")
+})
 basic.forever(function () {
     basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
         . . # . .
+        # # . # .
+        # # . . #
         `)
     basic.showLeds(`
         . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
         . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . #
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
         . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
         . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # # #
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
         . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . # # # #
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        # # # # #
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        # # # # .
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        # # # . .
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        # . # . .
+        # # . # .
+        # # . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
         `)
 })
